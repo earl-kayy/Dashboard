b341415d7cd7e3df6c385e7e9ed884a22db23b11
unknown <ryoonki@puzzle-ai.com>
2024-08-08 15:33:23 +0900

    Update packages

--- visual_tool.py
+++ visual_tool.py
@@ -3,9 +3,6 @@ import streamlit as st
 import plotly.graph_objects as go
 import json
 import altair as alt
-import seaborn as sns
-import pandas as pd
-import matplotlib.pyplot as plt
 
 class VisualTool:
 
